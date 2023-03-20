18467d2c8c7f96d74a4b2c6d31aa4d268bd0243c
Jonas Carlsen <egecarlsen@gmail.com>
2023-03-20 20:28:07 +0100

    Switch to tab-based terminal to allow for more easily having several terminals

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,16 +1,21 @@
+function _G.set_terminal_keymaps()
+	local opts = { buffer = 0 }
+	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 return {
 	"akinsho/toggleterm.nvim",
 	cond = not vim.g.vscode,
 	event = "VeryLazy",
-	config = function()
-		require("toggleterm").setup({
-			direction = "horizontal",
-			size = 60,
-			insert_mappings = false,
-			open_mapping = [[<leader>+]],
-			float_opts = {
-				border = "curved",
-			},
-		})
-	end,
+	opts = {
+		direction = "tab",
+		size = 60,
+		insert_mappings = false,
+		open_mapping = [[<leader>+]],
+		float_opts = {
+			border = "curved",
+		},
+	},
 }
