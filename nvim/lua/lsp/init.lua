beb95c2ba2763498e3a83ad8dba1bfcd76a914dc
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-03-21 16:45:29 +0100

    Fix react.d issue

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 local mason = require("mason-registry")
+local utils = require("util")
 
 M.default_capabilities = function()
 	return require("cmp_nvim_lsp").default_capabilities()
@@ -48,6 +49,32 @@ M.setup_servers = function()
 				plugins = {
 					M.get_ts_pkg("typescript-styled-plugin"),
 				},
+			},
+			handlers = {
+				["textDocument/definition"] = function(_, result, ...)
+					-- Filter out certain paths from the results that are 99% of the time
+					-- false positive results for my use case. If I explicitly jump to
+					-- them, go there, otherwise ignore them.
+					if vim.tbl_islist(result) then
+						local ignored_paths = {
+							"react/index.d.ts",
+							"tailwind-variants/dist/index.d.ts",
+						}
+
+						for key, value in ipairs(result) do
+							for _, ignored_path in pairs(ignored_paths) do
+								-- If an ignored path is the first result, keep it as it's
+								-- likely the intended path.
+								if key ~= 1 and utils.ends_with(value.targetUri, ignored_path) then
+									table.remove(result, key)
+								end
+							end
+						end
+					end
+
+					-- Defer to the built-in handler after filtering the results
+					vim.lsp.handlers["textDocument/definition"](_, result, ...)
+				end,
 			},
 		},
 	})
