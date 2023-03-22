b7e16addc26ad812edc4cf15d36c07810214b920
Jonas Carlsen <egecarlsen@gmail.com>
2023-03-23 00:01:08 +0100

    Simplify bindings for going to diagnostics

--- nvim/.config/nvim/lua/lsp/autocmd.lua
+++ nvim/.config/nvim/lua/lsp/autocmd.lua
@@ -19,7 +19,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set("n", "gr", "<cmd>Telescope lsp_definitions<cr>", opts)
 		vim.keymap.set("n", "<leader>.", vim.lsp.buf.code_action, opts)
 		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-		vim.keymap.set("n", "<leader>ge", vim.diagnostic.goto_next)
+		vim.keymap.set("n", "gn", vim.diagnostic.goto_next)
+		vim.keymap.set("n", "gp", vim.diagnostic.goto_next)
 
 		-- Only enable formatting for specific LSP's. Many LSPs that have built-in
 		-- formatting don't have robust enough formatting (e.g. tsserver is not as
