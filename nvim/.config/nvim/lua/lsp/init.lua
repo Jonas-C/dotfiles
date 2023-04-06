7485327c67473f63787a8118c6b2d360b2381da5
Jonas Carlsen <egecarlsen@gmail.com>
2023-04-06 14:05:53 +0200

    Sync. Remove dim as lsp supports it in nightly

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -32,6 +32,7 @@ M.setup_servers = function()
 	M.server("rust_analyzer")
 	M.server("yamlls")
 	M.server("eslint")
+	M.server("graphql")
 	M.server("jsonls", {
 		on_new_config = function(new_config)
 			new_config.settings.json.schemas = new_config.settings.json.schemas or {}
