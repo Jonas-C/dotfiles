5d6cc894fe576f353d8efe2d7a7a5180a6320d5e
Jonas Carlsen <egecarlsen@gmail.com>
2023-04-14 22:34:32 +0200

    Fix issue with cmd suggestions overlapping cmd line

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -19,48 +19,9 @@ return {
 		-- 	},
 		-- },
 		views = {
-			cmdline_popup = {
-				position = {
-					row = 5,
-					col = "50%",
-				},
-				size = {
-					width = 60,
-					height = "auto",
-				},
-			},
 			notify = {
 				enabled = false,
 			},
-			-- documentation = {
-			-- 	view = "hover",
-			-- 	---@type NoiceViewOptions
-			-- 	opts = {
-			-- 		lang = "markdown",
-			-- 		replace = true,
-			-- 		render = "plain",
-			-- 		format = { "{message}" },
-			-- 		win_options = { concealcursor = "n", conceallevel = 3 },
-			-- 	},
-			-- },
-			-- popupmenu = {
-			-- 	relative = "editor",
-			-- 	position = {
-			-- 		row = 8,
-			-- 		col = "50%",
-			-- 	},
-			-- 	size = {
-			-- 		width = 60,
-			-- 		height = 10,
-			-- 	},
-			-- 	border = {
-			-- 		style = "rounded",
-			-- 		padding = { 0, 1 },
-			-- 	},
-			-- 	win_options = {
-			-- 		winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
-			-- 	},
-			-- },
 		},
 		lsp = {
 			progress = {
