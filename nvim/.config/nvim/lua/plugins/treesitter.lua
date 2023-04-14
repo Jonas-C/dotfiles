4116a2f8a1e61baf3aa686a04cb30c72ece3b2da
Jonas Carlsen <egecarlsen@gmail.com>
2023-04-14 22:21:08 +0200

    Remove treesitter playground

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -55,24 +55,6 @@ return {
 					-- extended_mode = false,
 					-- termcolors = {},
 				},
-				playground = {
-					enable = true,
-					disable = {},
-					updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-					persist_queries = false, -- Whether the query persists across vim sessions
-					keybindings = {
-						toggle_query_editor = "o",
-						toggle_hl_groups = "i",
-						toggle_injected_languages = "t",
-						toggle_anonymous_nodes = "a",
-						toggle_language_display = "I",
-						focus_language = "f",
-						unfocus_language = "F",
-						update = "R",
-						goto_node = "<cr>",
-						show_help = "?",
-					},
-				},
 			})
 
 			-- Custom predicate to improve highlighting of languages injected into JS
@@ -105,10 +87,6 @@ return {
 				return false
 			end, true)
 		end,
-	},
-	{
-		"nvim-treesitter/playground",
-		event = "VeryLazy",
 	},
 	-- {
 	-- 	"andymass/vim-matchup",
