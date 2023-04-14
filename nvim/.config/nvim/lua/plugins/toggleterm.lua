37ecd3c5af277f68c6f691c9f91fd8a02f419ac9
Jonas Carlsen <egecarlsen@gmail.com>
2023-04-14 23:36:03 +0200

    Override regular gf when using it in terminal

--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -3,6 +3,27 @@ function _G.set_terminal_keymaps()
 	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
 end
 
+-- Close Terminal and open buffer in "normal window".
+-- https://github.com/akinsho/toggleterm.nvim/issues/116
+local function go_to_file()
+	local toggleterm = require("toggleterm")
+	local cursor = vim.api.nvim_win_get_cursor(0)
+	local bufnr = vim.api.nvim_get_current_buf()
+	toggleterm.toggle(0)
+	vim.api.nvim_win_set_buf(0, bufnr)
+	vim.api.nvim_win_set_cursor(0, cursor)
+	vim.cmd("norm! gf")
+end
+
+vim.api.nvim_create_augroup("ToggleTerm", {})
+vim.api.nvim_create_autocmd("TermOpen", {
+	pattern = "term://*toggleterm#*",
+	callback = function()
+		vim.keymap.set("n", "gf", go_to_file, { buffer = true })
+	end,
+	group = "ToggleTerm",
+})
+
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 return {
