ce182aae6c1d9e9a40f664c5ef191d712004f0d1
Jonas Carlsen <egecarlsen@gmail.com>
2023-05-13 16:30:33 +0200

    Clean up bufferline a bit

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -3,27 +3,15 @@ return {
 	cond = not vim.g.vscode,
 	event = "VeryLazy",
 	opts = {
-		icons_enabled = true,
 		options = {
 			disabled_filetypes = { "neo-tree" },
 		},
-		theme = "auto",
-		component_separators = "|",
-		section_separators = { left = "", right = "" },
-		always_divide_middle = true,
-		globalstatus = false,
 		sections = {
 			lualine_a = { "mode" },
-			lualine_b = {},
-			lualine_c = {
-				{
-					"diagnostics",
-					sections = { "error", "warn", "info", "hint" },
-					always_visible = true,
-				},
-			},
-			lualine_x = { "none" },
-			lualine_y = { "branch", "diff" },
+			lualine_b = { "branch", "diff" },
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = {},
 			lualine_z = {},
 		},
 		extensions = { "neo-tree" },
