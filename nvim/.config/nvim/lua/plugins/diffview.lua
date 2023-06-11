bbdd7dd28bd75d9cda7cfd3abc5dcfef9820e3e1
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-06-11 13:40:54 +0200

    Refactor DiffviewToggle to hopefully fix crash

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -1,29 +1,21 @@
-function DiffviewToggle()
-	local lib = require("diffview.lib")
-	local view = lib.get_current_view()
+vim.api.nvim_create_user_command("DiffviewToggle", function(e)
+	local view = require("diffview.lib").get_current_view()
+
 	if view then
-		-- Current tabpage is a Diffview; close it
-		vim.cmd(":DiffviewClose")
+		vim.cmd("DiffviewClose")
 	else
-		-- No open Diffview exists: open a new one
-		vim.cmd(":DiffviewOpen")
+		vim.cmd("DiffviewOpen " .. e.args)
 	end
-end
+end, { nargs = "*" })
 
 return {
 	"sindrets/diffview.nvim",
 	version = false,
 	cond = not vim.g.vscode,
 	keys = {
-		{ "<Leader>gg", ":lua DiffviewToggle()<cr>", { desc = "Show git diff" } },
+		{ "<Leader>gg", "<cmd>DiffviewToggle<cr>", { desc = "Show git diff" } },
 	},
-	cmd = {
-		"DiffviewOpen",
-		"DiffviewClose",
-		"DiffviewToggle",
-		"DiffviewFocusFiles",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
+	dependencies = { "nvim-lua/plenary.nvim" },
 	opts = {
 		enhanced_diff_hl = true,
 	},
