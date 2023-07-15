ca4db0c8f09155bff97af5b592e97e7e20c9af8b
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-07-15 10:38:53 +0200

    Update tsx queries

--- nvim/.config/nvim/queries/tsx/highlights.scm
+++ nvim/.config/nvim/queries/tsx/highlights.scm
@@ -5,7 +5,3 @@
  "declare"
  ] @keyword.declaration (#set! "priority" 2000)
 
-; Override @tag @constructor usage in default tsx query
-(jsx_opening_element ((nested_identifier (identifier) @constructor (identifier) @constructor)))
-(jsx_closing_element ((nested_identifier (identifier) @constructor (identifier) @constructor)))
-(jsx_self_closing_element ((nested_identifier (identifier) @constructor (identifier) @constructor)))
