2b42889c4d091cac9bc9785e134dc89bfa4ad135
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-08-08 23:17:17 +0200

    Lazy-load illuminate

--- nvim/.config/nvim/lua/plugins/illuminate.lua
+++ nvim/.config/nvim/lua/plugins/illuminate.lua
@@ -1,7 +1,13 @@
 return {
 	"RRethy/vim-illuminate",
+	event = { "BufReadPost", "BufNewFile" },
 	config = function()
 		require("illuminate").configure({
+			delay = 100,
+			large_file_cutoff = 2000,
+			large_file_overrides = {
+				providers = { "lsp" },
+			},
 			min_count_to_highlight = 2,
 		})
 	end,
