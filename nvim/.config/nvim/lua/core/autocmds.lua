1d7a87395c46b67016d188f1963dce0cda07fbb1
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-09-28 15:21:16 +0200

    Take some  autocmds from lazy

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -1,24 +1,38 @@
 local augroup = function(name)
-	return vim.api.nvim_create_augroup("tvl_" .. name, { clear = true })
+	return vim.api.nvim_create_augroup("ec" .. name, { clear = true })
 end
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = augroup("highlight_yank"),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
 
 -- Do not automatically comment newlines. Caused by ftplugin
 vim.api.nvim_create_autocmd("FileType", { command = "set formatoptions-=o" })
 
 -- Set conceallevel when entering a norg file. Enables fancy neorg functionality.
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, { pattern = { "*.norg" }, command = "set conceallevel=3" })
---
+
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd("FileType", {
 	group = augroup("close_with_q"),
 	pattern = {
-		"qf",
+		"PlenaryTestPopup",
 		"help",
+		"lspinfo",
 		"man",
 		"notify",
-		"lspinfo",
+		"qf",
+		"query",
+		"spectre_panel",
 		"startuptime",
-		"PlenaryTestPopup",
+		"tsplayground",
+		"neotest-output",
+		"checkhealth",
+		"neotest-summary",
+		"neotest-output-panel",
 	},
 	callback = function(event)
 		vim.bo[event.buf].buflisted = false
