8fec39c90cfa2d6355cf4d9fa30bfa35658fddb6
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-09-28 13:55:31 +0200

    Update how lazy is initialized

--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -1,23 +1,29 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  -- bootstrap lazy.nvim
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
-require("lazy").setup("plugins", {
+require("lazy").setup({
+	spec = {
+		{ import = "plugins" },
+	},
+	defaults = {
+		-- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+		-- have outdated releases, which may break your Neovim install.
+		version = false,
+	},
+	checker = { enabled = true }, -- automatically check for plugin updates
 	performance = {
 		rtp = {
+			-- disable some rtp plugins
 			disabled_plugins = {
 				"gzip",
+				-- "matchit",
+				-- "matchparen",
 				"netrwPlugin",
-				"rplugin",
 				"tarPlugin",
 				"tohtml",
 				"tutor",
