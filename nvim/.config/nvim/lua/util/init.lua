270e360e52b0e50179796836a30b1fc47d974af9
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-09-28 17:37:15 +0200

    Remove some unused util functions

--- nvim/.config/nvim/lua/util/init.lua
+++ nvim/.config/nvim/lua/util/init.lua
@@ -1,25 +1,5 @@
 local M = {}
 
-function M.opts(name)
-	local plugin = require("lazy.core.config").plugins[name]
-	if not plugin then
-		return {}
-	end
-	local Plugin = require("lazy.core.plugin")
-	return Plugin.values(plugin, "opts", false)
-end
-
----@param on_attach fun(client, buffer)
-function M.on_attach(on_attach)
-	vim.api.nvim_create_autocmd("LspAttach", {
-		callback = function(args)
-			local buffer = args.buf
-			local client = vim.lsp.get_client_by_id(args.data.client_id)
-			on_attach(client, buffer)
-		end,
-	})
-end
-
 --- Returns true when the provided string ends with the provided ending
 --- @param str string
 --- @param ending string
