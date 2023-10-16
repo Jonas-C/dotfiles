13d85e123cbeb926fac1184577b9bd4dc5295af9
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-10-16 22:13:18 +0200

    Clean up treesitter config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -11,8 +11,6 @@ return {
 				"RRethy/nvim-treesitter-endwise",
 				ft = { "bash", "lua" },
 			},
-			-- "HiPhish/nvim-ts-rainbow2",
-			"windwp/nvim-ts-autotag",
 			"JoosepAlviste/nvim-ts-context-commentstring",
 		},
 		config = function()
@@ -33,23 +31,12 @@ return {
 					enable = true,
 					enable_autocmd = false,
 				},
-				-- rainbow = {
-				-- 	-- I love rainbow highlights, but they're currently breaking DiffView.nvim
-				-- 	enable = false,
-				-- 	hlgroups = {
-				-- 		"TSRainbowYellow",
-				-- 		"TSRainbowViolet",
-				-- 		"TSRainbowCyan",
-				-- 	},
-				--
-				-- 	query = {
-				-- 		"rainbow-parens",
-				-- 		html = "rainbow-parens",
-				-- 		javascript = "rainbow-parens",
-				-- 		tsx = "rainbow-blocks",
-				-- 	},
-				-- },
 			})
 		end,
+	},
+	{
+		"windwp/nvim-ts-autotag",
+		event = "InsertEnter",
+		opts = {},
 	},
 }
