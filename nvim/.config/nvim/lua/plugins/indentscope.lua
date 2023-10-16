f2d767c79e300880eee964be82fbb21274028dec
Jonas Ege Carlsen <jonas.carlsen@knowit.no>
2023-10-17 00:30:18 +0200

    Disable indentscope some places

--- nvim/.config/nvim/lua/plugins/indentscope.lua
+++ nvim/.config/nvim/lua/plugins/indentscope.lua
@@ -1,5 +1,19 @@
 return {
 	"echasnovski/mini.indentscope",
+	init = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = {
+				"help",
+				"neo-tree",
+				"lazy",
+				"mason",
+				"toggleterm",
+			},
+			callback = function()
+				vim.b.miniindentscope_disable = true
+			end,
+		})
+	end,
 	config = function()
 		require("mini.indentscope").setup({
 			draw = {
