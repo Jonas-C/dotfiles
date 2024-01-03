0c17275abfd2e23725c4baeef31d3673af75994f
Jonas Carlsen <egecarlsen@gmail.com>
2024-01-03 22:17:16 +0100

    Remove unused import

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -47,7 +47,6 @@ return {
 		opts = function()
 			vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
 			local cmp = require("cmp")
-			local defaults = require("cmp.config.default")()
 
 			return {
 				completion = {
