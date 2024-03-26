3043ed3e67f98fae94fdece9f352212501faf6d2
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-03-26 15:13:02 +0100

    Remove LuaSnip. I don't use snippets, and nvim-cmp apparently doesn't require it anymore

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,39 +1,5 @@
 return {
 	{
-		"L3MON4D3/LuaSnip",
-		lazy = true,
-		cond = not vim.g.vscode,
-		opts = {
-			history = true,
-			delete_check_events = "TextChanged",
-		},
-		keys = {
-			{
-				"<tab>",
-				function()
-					return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-				end,
-				expr = true,
-				silent = true,
-				mode = "i",
-			},
-			{
-				"<tab>",
-				function()
-					require("luasnip").jump(1)
-				end,
-				mode = "s",
-			},
-			{
-				"<s-tab>",
-				function()
-					require("luasnip").jump(-1)
-				end,
-				mode = { "i", "s" },
-			},
-		},
-	},
-	{
 		"hrsh7th/nvim-cmp",
 		lazy = true,
 		version = false,
