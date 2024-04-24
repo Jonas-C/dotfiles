77fadf71ab4fd4cf64716e6e1cae143b8783c83b
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-04-24 20:13:22 +0200

    Clean up neogit config

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -7,12 +7,6 @@ return {
 		{ "<leader>x", "<cmd>Neogit<cr>", { desc = "Open Neogit" } },
 	},
 	opts = {
-		use_telescope = true,
-		-- There's currently a bug that breaks neogit when used with noice. Disable confirmation for now.
-		disable_commit_confirmation = true,
-		telescope_sorter = function()
-			return require("telescope").extensions.fzf.native_fzf_sorter()
-		end,
 		integrations = {
 			diffview = true,
 		},
