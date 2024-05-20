63d89f7c180e895291d8a6727dfeeba57db7a4ca
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-05-20 19:51:43 +0200

    Fix luarocks dep in neorg

--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-neorg/neorg",
 	ft = "norg",
-	dependencies = { "luarocks.nvim" },
+	dependencies = { "vhyrro/luarocks.nvim" },
 	opts = {
 		load = {
 			["core.defaults"] = {}, -- Loads default behaviour
