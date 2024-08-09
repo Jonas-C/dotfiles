34007e82aec5420fa42f04a700795aa73ed253ec
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-08-09 16:49:51 +0200

    Enable markdown export

--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -5,6 +5,7 @@ return {
 		load = {
 			["core.defaults"] = {}, -- Loads default behaviour
 			["core.concealer"] = {}, -- Adds pretty icons to your documents
+			["core.export"] = {},
 			["core.completion"] = {
 				config = {
 
