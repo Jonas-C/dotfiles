c4ad0455b725fa326e09194623d14dfac0e97a4c
Jonas Carlsen <jonas.carlsen@knowit.no>
2024-11-09 14:42:13 +0100

    enable some markdown stuff to enable proper markdown rendering when using blink

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,5 +1,19 @@
 return {
 	"MeanderingProgrammer/render-markdown.nvim",
-	opts = {},
+	opts = {
+		overrides = {
+			buftype = {
+				nofile = {
+					render_modes = { "n", "c", "i" },
+					debounce = 5,
+					-- code = {
+					-- 	left_pad = 0,
+					-- 	right_pad = 0,
+					-- 	language_pad = 0,
+					-- },
+				},
+			},
+		},
+	},
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 }
