ccab7583ce886b13a9c1ab3f0d4c4091de9fc03e
Jonas Carlsen <jonas.carlsen@knowit.no>
2025-03-04 18:32:15 +0100

    temporary norg changes

--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -1,17 +1,13 @@
 return {
 	"nvim-neorg/neorg",
-	ft = "norg",
+	lazy = false,
 	opts = {
 		load = {
 			["core.defaults"] = {}, -- Loads default behaviour
 			["core.concealer"] = {}, -- Adds pretty icons to your documents
 			["core.export"] = {},
-			["core.completion"] = {
-				config = {
-
-					engine = "nvim-cmp",
-				},
-			},
+			-- This is required for now, breaks on nightly
+			["core.esupports.metagen"] = { config = { update_date = false } },
 			["core.dirman"] = { -- Manages Neorg workspaces
 				config = {
 					workspaces = {
