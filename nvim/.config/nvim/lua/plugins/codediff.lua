374dae74cf49d1cd76c8612a30b8ec51e68bcd9d
Jonas Carlsen <jonas.carlsen@knowit.no>
2026-02-28 14:33:05 +0100

    try out codediff

---
+++ nvim/.config/nvim/lua/plugins/codediff.lua
@@ -0,0 +1,9 @@
+return {
+	"esmuellert/codediff.nvim",
+	dependencies = { "MunifTanjim/nui.nvim" },
+	cmd = "CodeDiff",
+	lazy = true,
+	-- keys = {
+	-- 	{ "<Leader>gg", "<cmd>CodeDiff<cr>", desc = "Show git diff" },
+	-- },
+}
